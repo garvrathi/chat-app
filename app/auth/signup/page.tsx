80960fcee26e44c10a667e175b0b4e2a609c1118
garvrathi <garvrathi2004@gmail.com>
2024-08-04 09:57:13 +0530

    Init

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div className="flex">
+      <div className="bg-[url('/images/bg2.jpg')] bg-no-repeat bg-cover h-screen hidden lg:block lg:w-[50%] xl:w-[65%]"></div>
+      <div className="flex-1 flex h-screen justify-center items-center px-4">
+        <form className="w-[90%]">
+          <h1 className="capitalize text-xl font-semibold text-white mb-8">
+            new user? Signup Here!
+          </h1>
+          <div>
+            <input
+              type="text"
+              name="name"
+              placeholder="name..."
+              className="w-full h-14 rounded-lg outline-none bg-[#363738] text-white px-4"
+            />
+          </div>
+          <div className="mt-6">
+            <input
+              type="text"
+              name="email"
+              placeholder="email..."
+              className="w-full h-14 rounded-lg outline-none bg-[#363738] text-white px-4"
+            />
+          </div>
+          <div className="mt-6">
+            <input
+              type="password"
+              name="password"
+              placeholder="create password..."
+              className="w-full h-14 rounded-lg outline-none bg-[#363738] text-white px-4"
+            />
+          </div>
+          <div className="mt-6">
+            <input
+              type="submit"
+              value="signup"
+              className="w-full block cursor-pointer bg-[#0071db] text-white text-lg font-semibold capitalize rounded-lg h-14 px-4"
+            />
+          </div>
+          <Link
+            href="/auth/signin"
+            className="inline-block mt-4 text-zinc-400 font-semibold hover:text-white focus:text-white"
+          >
+            Already have an account?
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default page;
